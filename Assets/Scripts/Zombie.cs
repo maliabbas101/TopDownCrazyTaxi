d66234bad6dbab86f9c28aff129c895a1d0a0aa8
Muhammad Ali Abbas <97804868+maliabbas101@users.noreply.github.com>
2024-10-18 23:51:34 +0400

    Modify the zombie movement.

--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -1,24 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Zombie : MonoBehaviour
 {
-    public float moveSpeed = 100f;
+    public float moveSpeed = 0.001f;
     public float rotationSpeed = 100f;
+    [SerializeField] PrometeoCarController player;
+    private Vector3 directionToHead;
+    private float angleToRotae;
 
     void Start()
     {
-        StartCoroutine(Wander());
+        //StartCoroutine(Wander());
     }
+    private void Update()
+    {
+        directionToHead = (player.transform.position - transform.position).normalized;
+        if (Vector3.Distance(transform.position, player.transform.position)> 5f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, Time.deltaTime * 2);
+            angleToRotae = Mathf.Atan2(directionToHead.y, directionToHead.x) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(new Vector3(0,angleToRotae,0));
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime);
+        }
+        
 
+    }
     IEnumerator Wander()
     {
         while (true)
         {
-            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
-
-            yield return new WaitForSeconds(Random.Range(1f, 3f));
+            yield return new WaitForSeconds(Random.Range(1f,1.1f));
             float randomRotation = Random.Range(-rotationSpeed, rotationSpeed);
             transform.Rotate(Vector3.up, randomRotation);
         }
